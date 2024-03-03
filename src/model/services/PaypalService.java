0cd6d0ac2a7167fa04b20aa4e3d74c8e73c66798
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-03-03 22:44:32 +0100

    🔧 Add microservices

---
+++ src/model/services/PaypalService.java
@@ -0,0 +1,12 @@
+package model.services;
+
+public class PaypalService implements OnlinePaymentServices{
+    
+    public Double paymentFee(Double amount) {
+
+    }
+    public Double interest(Double amount, Integer months) {
+
+    }
+
+}
