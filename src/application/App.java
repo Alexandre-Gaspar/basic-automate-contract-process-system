835a03ae7820b02adb03daae1a314a2d3c1d1ba2
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-03-07 05:52:07 +0100

    ✨ Add principal class the has the main method

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,43 @@
 package application;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+// import model.services.PaypalService;
+
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("\n\nEnter the contract datas");
+        System.out.print("Number: ");
+        Integer contractNumber = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        sc.nextLine(); // Clear the buffer memory after clicking ENTER
+        LocalDate contractDate = LocalDate.parse(sc.nextLine(), formatDate);
+        System.out.print("Contract value: ");
+        Double contractValue = sc.nextDouble();
+
+        Contract contract = new Contract(contractNumber, contractDate, contractValue); // Create a contract
+
+        ContractService service = new ContractService();
+
+        System.out.print("Enter the number of installments: ");
+        Integer installmentNumber = sc.nextInt();
+
+        service.processContract(contract, installmentNumber);
+
+        System.out.println("\nParcelas:");
+        
+        System.out.println(contract.toString());
+
     }
 }
