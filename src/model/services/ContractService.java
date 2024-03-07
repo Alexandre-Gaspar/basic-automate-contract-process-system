30c0c16b67b2d96a4fa1fbc03a227b8d23ffb2e9
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-03-07 05:45:54 +0100

    👔 Update the business logic to the services

--- src/model/services/ContractService.java
+++ src/model/services/ContractService.java
@@ -1,21 +1,26 @@
 package model.services;
 
-import model.entities.Contract;
+import java.time.LocalDate;
 
-public class ContractService {
-    
+import model.entities.Contract;
+import model.entities.Installment;
+
+public class ContractService extends PaypalService {
+
     public void processContract(Contract contract, Integer months) {
 
         Double amountPerInstallment = contract.getTotalValue() / months;
-        OnlinePaymentServices service = new PaypalService();
 
         int count = 0;
         do {
-            ++months;
-            service.interest(amountPerInstallment, months);
-            contract.addInstallment(null);
+            ++count;
+            Double simpleMonthlyInterest = amountPerInstallment + super.interest(amountPerInstallment, count);
+            Double totalValuePerInstallment = simpleMonthlyInterest + super.paymentFee(simpleMonthlyInterest);
+            LocalDate dateWithNextMonth = contract.getDate().plusMonths(count);
+
+            contract.addInstallment(new Installment(dateWithNextMonth, totalValuePerInstallment));
         } while (count < months);
-        
+
     }
 
 }
